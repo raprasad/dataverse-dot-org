fd62121b27c211d83b234db852d989e50e3e76ea
Raman Prasad <raman_prasad@harvard.edu>
2014-09-18 22:07:33 -0400

    add pager tests

--- thedata/apps/dvcards/display_pager.py
+++ thedata/apps/dvcards/display_pager.py
@@ -1,11 +1,28 @@
+"""
+python manage.py test apps.dvcards.tests
+python apps/dvcards/display_pager.py
+"""
 
 
 class DisplayPager:
-    
-    def __init__(self, num_hits, items_per_page=10, result_start_offset=0):
+    """
+    This will be used to create the HTML results pager.
+    """
+    def __init__(self, num_hits, items_per_page=10, result_start_offset=0, **kwargs):
+        assert(num_hits >= 0)
         assert(items_per_page >= 1)
         assert(result_start_offset >= 0)
         
+        self.num_pager_buttons = kwargs.get('num_pager_buttons', 5)
+        
+        assert(self.num_pager_buttons > 0)
+        # make sure offset doesn't exceed num_hits
+        # possible that query results could change (solr cleared, etc)
+        #
+        if (result_start_offset+1) > num_hits:
+            result_start_offset = 0
+        
+        # Should the pager be used at all
         self.show_pager = False     # No need to show the page if only 1 page is available
 
         # Numbers to start with
@@ -13,14 +30,11 @@ class DisplayPager:
         self.items_per_page = items_per_page
         self.result_start_offset = result_start_offset
         
-        self.current_page = 1#self.get_current_page()
-        # figure out current page from result_start_offset
-        
-        
-        self.num_page_buttons = 5
+        self.current_page = 0
+    
         
         # to calculate
-        self.num_pages = 0
+        self.page_count = 0
 
         self.first_page = None
         self.previous_page = None
@@ -38,29 +52,33 @@ class DisplayPager:
         assert(type(page_num) is int)
         
         if self.current_page == page_num:
-            return '<b>%s</b>' % page_num
+            return '<em>%s</em>' % page_num
         
         return page_num
         
+    def get_page_menu_with_chosen(self):
+        if self.page_menu_range is None:
+            return None
+        return [self.em_current_page(x) for x in self.page_menu_range]
+        
     def show(self):
         
-        page_menu = [self.em_current_page(x) for x in self.page_menu_range]
+        page_menu = self.get_page_menu_with_chosen()
         
         test_str = """
 ------------
 page settings
 ------------
 current page: %s
-
 num pages: %s 
-
+num_pager_buttons: %s
 menu: %s
-
 first|prev|next|last: %s | %s | %s | %s
 ------------
 
 """ % (self.current_page\
-    , self.num_pages\
+    , self.page_count\
+    , self.num_pager_buttons
     , page_menu
     , self.first_page
     , self.previous_page
@@ -71,6 +89,9 @@ first|prev|next|last: %s | %s | %s | %s
         
     def get_current_page(self):
         # start on page 1
+        if self.num_hits == 0:
+            return 0
+            
         if self.result_start_offset == 0:    
             return 1
         
@@ -79,11 +100,12 @@ first|prev|next|last: %s | %s | %s | %s
             return 1
 
         # calculate page number based on offset
-        current_page_num = self.result_start_offset / self.items_per_page
-        if (self.result_start_offset % self.items_per_page) > 1:
+        current_page_num = (self.result_start_offset + 1) / self.items_per_page
+        if ( (self.result_start_offset + 1) % self.items_per_page) > 0:
                current_page_num += 1 
         return current_page_num
         
+    '''
     def get_solr_offset(self):
         """
         if items_per_page == 10:
@@ -95,15 +117,30 @@ first|prev|next|last: %s | %s | %s | %s
             self.calculate_pager_items()
             
         return (self.current_page - 1) * (self.items_per_page)
+    '''
     
+    def set_page_count(self):
+        """Set the number of pages"""
+
+        if self.num_hits == 0: # No hits, no pages
+            self.page_count = 0
+        else:
+            # number of full pages
+            self.page_count = (self.num_hits / self.items_per_page) 
+            # is there a partial page
+            if ( self.num_hits % self.items_per_page) > 0:
+                self.page_count += 1
+        
     def calculate_pager_items(self):
         if self.num_hits >= 0 and self.num_hits <= self.items_per_page:
             # no need for a pager
             return 
-            
-        self.num_pages = (self.num_hits / self.items_per_page) 
-        if ( self.num_hits % self.items_per_page) > 0:
-            self.num_pages += 1
+        
+        self.set_page_count()
+        
+        # Do we need the pager to display
+        if self.page_count > 0:
+            self.show_pager = True
         
         self.current_page = self.get_current_page()
         
@@ -113,18 +150,18 @@ first|prev|next|last: %s | %s | %s | %s
             self.first_page = 1
             self.previous_page = self.current_page - 1
         
-        if self.current_page < self.num_pages:
-            self.last_page = self.num_pages
+        if self.current_page < self.page_count:
+            self.last_page = self.page_count
             self.next_page = self.current_page + 1
         
         # list of page menu numbers to display
         #
         odd_pages = False
-        if (self.num_page_buttons % 2) > 0:
+        if (self.num_pager_buttons % 2) > 0:
             odd_pages = True
 
         # how many page buttons do we need
-        page_button_count = min(self.num_pages, self.num_page_buttons)
+        page_button_count = min(self.page_count, self.num_pager_buttons)
         
         # calculate the mid_button
         if odd_pages: 
@@ -137,23 +174,45 @@ first|prev|next|last: %s | %s | %s | %s
             num_right = page_button_count / 2 
         
         # calculate the page range
-        if False:#self.current_page < mid_button:
-            self.page_menu_range =  [999]
-            pass # for now
-            #self.page_menu_range = range(1, mid_button) + range(mid_button, page_button_count+1)    
-        else:# odd_pages:
-            leftmost_page = self.current_page - num_left
-            rightmost_page = self.current_page + num_right
-            self.page_menu_range = range(leftmost_page, self.current_page) \
-                            + [self.current_page]\
-                            + range(self.current_page+1, rightmost_page + 1)
-            
+
+        leftmost_page = self.current_page - num_left
+        leftmost_page = max(leftmost_page, 1)   # should not be less than 1
+        
+        rightmost_page = self.current_page + num_right
+        rightmost_page = min(rightmost_page, self.page_count)   # should not be less than 1
+        
+        left_page_range = range(leftmost_page, self.current_page)
+        right_page_range = range(self.current_page+1, rightmost_page + 1)
+
+        # Is left page range filled out? If not, add right pages
+        #
+        num_left_unused = num_left - len(left_page_range) 
+        while num_left_unused > 0:
+            if len(right_page_range) == 0 or right_page_range[-1] == self.page_count:
+                break
+            right_page_range.append(right_page_range[-1] + 1)
+            num_left_unused = num_left_unused - 1
+
+        # Is right page range filled out? If not, add left pages
+        #
+        num_right_unused = num_right - len(right_page_range) 
+        while num_right_unused > 0:
+            if len(left_page_range) == 0 or left_page_range[0] == 1:
+                break
+            left_page_range.insert(0, left_page_range[0] - 1)
+            num_right_unused = num_right_unused - 1
+        
+        self.page_menu_range = left_page_range\
+                        + [self.current_page]\
+                        + right_page_range
+        
 
         self.pager_calculated = True
 
 if __name__=='__main__':
-    p = DisplayPager(num_hits=26, items_per_page=5, result_start_offset=21)
+    kwargs = dict(num_pager_buttons=5)
+    p = DisplayPager(num_hits=70, items_per_page=10, result_start_offset=71, **kwargs)
     p.show()
-    #print (p.num_pages)
+    #print (p.page_count)
     #print (p.page_menu_range)
     
